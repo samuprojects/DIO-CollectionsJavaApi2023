8662bbe13b37f27a4ffb325b8cf753abbb0a7e15
samuel <samugit.projects@gmail.com>
2023-09-06 10:43:00 -0300

    Pesquisa Set

---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,43 @@
+package main.java.set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings("ALL")
+public class AgendaContatos {
+
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato (String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatoSet){
+            if (c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato (String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet){
+            if (c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+}
