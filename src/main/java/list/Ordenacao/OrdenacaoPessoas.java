28a248278dac705da06da93ca595e0e3e8b26937
samuel <samugit.projects@gmail.com>
2023-09-05 21:03:00 -0300

    Ordenação List: testando os métodos

--- src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -28,4 +28,17 @@ public class OrdenacaoPessoas {
         Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
         return pessoasPorAltura;
     }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.pessoaList);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
 }
