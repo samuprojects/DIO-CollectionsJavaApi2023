c6973043beb6e608bd84dd6135d0af49efa16d28
samuel <samugit.projects@gmail.com>
2023-09-08 16:21:10 -0300

    Pesquisa Map - testando os métodos

--- src/main/java/map/Pesquisa/EstoqueProdutos.java
+++ src/main/java/map/Pesquisa/EstoqueProdutos.java
@@ -71,4 +71,28 @@ public class EstoqueProdutos {
         }
         return produtoMaiorQuantidadeValorNoEstoque;
     }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
+        estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
+        estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
+        estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais caro: " + produtoMaisCaro);
+
+        Produto produtoMaisBarato = estoque.obterProdutoMaisBarato();
+        System.out.println("Produto mais barato: " + produtoMaisBarato);
+
+        Produto produtoMaiorQuantidadeValorTotal = estoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque();
+        System.out.println("Produto com maior quantidade em valor no estoque: " + produtoMaiorQuantidadeValorTotal);
+    }
 }
