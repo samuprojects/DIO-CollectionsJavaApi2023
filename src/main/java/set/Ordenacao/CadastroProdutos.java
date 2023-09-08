37db2b0f262aead99191a60662ff5b32cc276af5
samuel <samugit.projects@gmail.com>
2023-09-07 22:19:22 -0300

    Ordenação Set

---
+++ src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,30 @@
+package main.java.set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+@SuppressWarnings("ALL")
+public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtoSet.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+}
